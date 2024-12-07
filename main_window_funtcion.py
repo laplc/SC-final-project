9c8e6d26beb3056a58b49abc22e3e2d8d862b022
laplc <gardeniaxiao@gmail.com>
2024-12-07 18:45:37 -0500

    now we can save content into a database

--- main_window_funtcion.py
+++ main_window_funtcion.py
@@ -1,8 +1,8 @@
-import sys
+import sys, sqlite3
+from datetime import datetime
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from main_window import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from archive_window import archive_MainWindow
 from dashboard_window_function import func_dashboardwindow
 from archive_window_function import func_archive_MainWindow
 
@@ -11,24 +11,57 @@ class Func_MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)  
 
-        #connect signals to functions
-        self.add_text.clicked.connect(self.add_text_to_dashboard)
+        #connect signals to functions-page1
         self.archive_button.clicked.connect(self.pop_archive_window)
         self.dashboard_button.clicked.connect(self.pop_dashboard_window)
+        self.add_text.clicked.connect(self.getText)
 
-    def add_text_to_dashboard(self):
-        #when "save for later button" is clicked, save text to database
-        print("being clicked")
+        #connect signals to functions -page2
+
     
     def pop_archive_window(self):
-        #when "archive"is clicked, pop archive window
+        '''when "archive"is clicked, pop archive window'''
         self.subwindow = func_archive_MainWindow()
         self.subwindow.show()
 
     def pop_dashboard_window(self):
-        #when "dashboard"is clicked, pop dashboard window
+        '''when "dashboard"is clicked, pop dashboard window'''
         self.subwindow = func_dashboardwindow()
         self.subwindow.show()
+
+    def getText(self):
+        '''       
+        when "save or later" button is clicked, save this into database:
+            1-the content
+            2-the time
+        '''        
+        text = self.textEdit.toPlainText()
+
+        if text:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            conn = sqlite3.connect('dashboard.db')
+            cursor = conn.cursor()
+
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS dashboard (
+                id INTEGER PRIMARY KEY,
+                content TEXT NOT NULL,
+                time TEXT NOT NULL
+            )
+            ''')
+
+            cursor.execute('''
+            INSERT INTO dashboard (content, time)
+            VALUES(?,?)
+            ''',
+            (text, current_time))
+
+            conn.commit()
+            conn.close()
+
+            self.textEdit.clear()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
