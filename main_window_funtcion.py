5f3a24188ac58522748361b5e9d937fd0bcf5549
laplc <gardeniaxiao@gmail.com>
2024-12-09 17:26:02 -0500

    backup for test calendar

--- main_window_funtcion.py
+++ main_window_funtcion.py
@@ -2,10 +2,10 @@ import sys, sqlite3
 from datetime import datetime
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from main_window_1 import Ui_MainWindow
-from PyQt5.QtWidgets import QListWidgetItem, QCheckBox, QWidget, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QListWidgetItem
 from dashboard_window_function import func_dashboardwindow
 from archive_window_function import archive_window_function
-from PyQt5.QtGui import QColor, QBrush
+from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
 from TaskItem import TaskItemWithCheckbox, TaskItemWithoutCheckbox
 
